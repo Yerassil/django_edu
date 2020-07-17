5e404dc8d001a215d1887f49faa4277379a82fc3
Yerassil <yerassil@gmail.com>
2020-07-17 20:51:18 +0600

    Add mixins from django-braces

--- courses/views.py
+++ courses/views.py
@@ -8,6 +8,21 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
 
 
 class OwnerMixin(object):
